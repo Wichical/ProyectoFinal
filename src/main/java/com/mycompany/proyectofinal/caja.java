1a7d6e99ceafd449ece6734be634c27ae0f512e6
Wichical <wichicalduck89@gmail.com>
2026-07-21 19:55:51 -0600

    caja.java

---
+++ src/main/java/com/mycompany/proyectofinal/caja.java
@@ -0,0 +1,19 @@
+public class Caja {
+
+    private int numero;
+    private Cola cola;
+
+    public Caja(int numero) {
+        this.numero = numero;
+        this.cola = new Cola();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Cola getCola() {
+        return cola;
+    }
+
+}
